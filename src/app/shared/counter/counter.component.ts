078e26127eccfc5227d9dbbb084ab40be140e233
julianqui1 <julianquintero0111@gmail.com>
2024-09-28 20:58:23 -0500

    counter logic

--- src/app/shared/counter/counter.component.ts
+++ src/app/shared/counter/counter.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.css']
+  styleUrls: ['./counter.component.css'],
 })
 export class CounterComponent {
+  machineScore: number = 0;
+  humanScore: number = 0;
 
+  incrementMachineScore() {
+    this.machineScore++;
+  }
+
+  incrementHumanScore() {
+    this.humanScore++;
+  }
+
+  resetScores() {
+    this.machineScore = 0;
+    this.humanScore = 0;
+  }
 }
